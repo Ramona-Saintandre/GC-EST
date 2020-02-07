1189b9df834c321f7734c5679696dd971a7260d0
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-07 17:26:06 -0500

    started going through loopy loops

--- LoopyLoops/forloops.js
+++ LoopyLoops/forloops.js
@@ -3,6 +3,14 @@
 // Counting Loops:
 // Use a for loop to log numbers from 1 to 10, then another for loop to count down, logging
 // numbers from 10 to 1.
+
+
+    for( i = 0; <= 10; i++){
+        console.log(i);
+    }
+
+
+
 // Repeat the exercise with a while loop.
 // Repeat with a do while.
 // Create this array: const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Use a for … of to loop through
@@ -11,3 +19,15 @@
 // Given any string, use a loop to add padding (extra spaces) to the front to make the string ten
 // characters long. For example, given the string “planet”, log “ planet” (add four spaces), and for
 // the string “headlamp”, log “ headlamp” (add two spaces).
+
+
+// https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_loops2
+// Create a loop that runs through each item in the fruits array.
+
+
+var fruits = ["Apple", "Banana", "Orange"];
+for (x 
+ 
+) {
+  console.log(x);
+}
