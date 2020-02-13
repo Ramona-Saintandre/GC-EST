20c58626b562e9319b039edb630a7736318ad4a5
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-13 16:59:00 -0500

    looped  through a array

--- LoopyLoops/forloops.js
+++ LoopyLoops/forloops.js
@@ -34,6 +34,12 @@
 
 // Create this array: const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Use a for … of to loop through
 // this array and log all the numbers. (Note: we’re not asking you to also count down on this one.)
+//works
+// let num = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = 0; i <= num.length; i++){
+//     console.log (num[i]);
+// }
+
 
 // Extended Challenges second exercise:
 // Given any string, use a loop to add padding (extra spaces) to the front to make the string ten
